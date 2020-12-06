8eea3e8212fe376de1e890b30b1ca88f303a7ff8
Long Nguyen <lbn2@linux-a2.ews.illinois.edu>
2020-12-06 00:51:58 -0600

    changed user input from terminal to txt files

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 #include "openflights.h"
 
 using namespace std;
@@ -7,26 +8,28 @@ using namespace std;
 int main(){
     OpenFlights newFlights("source/airports.dat", "source/routes.dat");
 
+    // Read in input file
+    ifstream infile;
+    infile.open("input.txt");
+
     // Get starting location
     string startLocation;
     int start = -1;  // currently no valid input for starting location
     do {
-    cout<<"Please enter your starting location: ";
-    getline(cin, startLocation);
+    getline(infile, startLocation);
     start = newFlights.convertToId(startLocation);  //  convert location to airport ID
-    if (start < 0)  // checks if there is a valid input for starting location
-        cout<<"Sorry, that airport does not seem to exist"<<endl;
+    //if (start < 0)  // checks if there is a valid input for starting location
+    //    cout<<"Sorry, that airport does not seem to exist"<<endl;
     } while (start < 0);
 
     // Get final destination
     string destLocation;
     int dest = -1;  // currently no valid input for destination
     do {
-    cout<<"Please enter your final destination: ";
-    getline(cin, destLocation);
+    getline(infile, destLocation);
     dest = newFlights.convertToId(destLocation);  //  convert location to airport ID
-    if (dest < 0)  // checks if there is a valid input for destination
-        cout<<"Sorry, that airport does not seem to exist"<<endl;
+    //if (dest < 0)  // checks if there is a valid input for destination
+    //    cout<<"Sorry, that airport does not seem to exist"<<endl;
     } while (dest < 0);
     cout<<endl;
 
@@ -46,6 +49,23 @@ int main(){
     cout<<get<0>(dijkstra)[get<0>(dijkstra).size()-1]<<endl;
     cout<<"\nTotal distance of flights: "<<get<1>(dijkstra)<<" kilometers"<<endl;
     cout<<endl;
+
+    // Write to output file
+    ofstream outfile;
+    outfile.open("output.txt");
+
+    outfile << "For the least number of connecting flights, the airports you should fly to are: "<<endl;
+    for (size_t i=0; i<bfs.size()-1; i++)
+        outfile << bfs[i] << " -> ";
+    outfile<<bfs[bfs.size()-1]<<endl;
+    outfile<<endl;
+
+    outfile << "For the shortest distance traveled, the airports you should fly to are: "<<endl;
+    for (size_t i=0; i<get<0>(dijkstra).size()-1; i++)
+        outfile<<get<0>(dijkstra)[i]<<" -> ";
+    outfile<<get<0>(dijkstra)[get<0>(dijkstra).size()-1]<<endl;
+    outfile<<"\nTotal distance of flights: "<<get<1>(dijkstra)<<" kilometers"<<endl;
+    outfile<<endl;
 
     return 0;
 }
